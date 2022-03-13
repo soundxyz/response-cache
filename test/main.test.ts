0dadc40ffe555189303a65e8426f4ca45224c3c1
Pablo Sáez <pablosaez1995@gmail.com>
2022-03-12 17:26:59 -0800

    adjust tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,13 +2,16 @@ import test from "ava";
 import { execSync } from "child_process";
 import { gql, Plugin } from "graphql-ez";
 import IORedis from "ioredis";
-import { setTimeout } from "timers/promises";
 import RedLock from "redlock";
+import { setTimeout } from "timers/promises";
+import { inspect } from "util";
 
 import { CreateTestClient, GlobalTeardown } from "@graphql-ez/fastify-testing";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
 import { createRedisCache, useResponseCache } from "../src";
+
+inspect.defaultOptions.depth = Infinity;
 
 let redis: IORedis.Redis;
 let redLock: RedLock;
@@ -62,15 +65,18 @@ test.before(async () => {
 test.after.always(GlobalTeardown);
 
 test("hello", async (t) => {
-  const sharedCachePlugin = createCachePlugin();
   const data = await Promise.all(
-    new Array(10)
-      .fill(0)
-      .map(async (_, index) =>
-        (
-          await TestClient(index > 55 ? createCachePlugin() : sharedCachePlugin)
-        ).assertedQuery("{hello}")
-      )
+    new Array(10).fill(0).map(async (_, _index) => {
+      const testClient = await TestClient(createCachePlugin());
+
+      const result = await Promise.all([
+        testClient.assertedQuery("{hello}"),
+        testClient.assertedQuery("{hello}"),
+        testClient.assertedQuery("{hello}"),
+      ]);
+
+      return result;
+    })
   );
 
   console.log({
