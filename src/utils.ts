d5f97cf174411ef421666c2fa09f305603d9e955
Pablo Sáez <pablosaez1995@gmail.com>
2023-01-13 12:18:15 -0300

    concurrencyLimit option, by default 10.

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export function chunk<T>(array: T[], size: number) {
+  const chunked: T[][] = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunked.push(array.slice(i, i + size));
+  }
+  return chunked;
+}
