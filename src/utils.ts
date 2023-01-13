6942d021afa25033e414eb1a7ca93306c425decd
Pablo Sáez <pablosaez1995@gmail.com>
2023-01-13 12:36:41 -0300

    fix size <= 0

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 export function chunk<T>(array: T[], size: number) {
   const chunked: T[][] = [];
+  if (size <= 0) return [array];
   for (let i = 0; i < array.length; i += size) {
     chunked.push(array.slice(i, i + size));
   }
--- test/chunk.test.ts
+++ test/chunk.test.ts
@@ -8,4 +8,9 @@ test("chunks correctly", () => {
     [5, 6],
     [7, 8],
   ]);
+
+  expect(chunk([], 10)).toStrictEqual([]);
+  expect(chunk([1, 2, 3], 10)).toStrictEqual([[1, 2, 3]]);
+
+  expect(chunk([1, 2, 3], 0)).toStrictEqual([[1, 2, 3]]);
 });
